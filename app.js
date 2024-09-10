7ea552caf3658287b4aed0fec8f7b3322432d184
ZivAvraham76 <zivavraham76@gmail.com>
2024-09-10 15:49:54 +0300

    upload question answer collapse

---
+++ app.js
@@ -0,0 +1,8 @@
+let btns = document.querySelectorAll('.wrap-toggle');
+let wraps = document.querySelectorAll('.wrap');
+
+for(let x = 0; x < btns.length; x++){
+    btns[x].addEventListener('click', function(){
+        wraps[x].classList.toggle('active');
+    })
+}
